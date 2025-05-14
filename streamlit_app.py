1d48a37704eb142c39319b1199b77fbacdc87f06
ShenyEka <113080415+shenyeka@users.noreply.github.com>
2025-05-15 06:43:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -966,4 +966,4 @@ elif menu == "PEMODELAN ARIMA-ANFIS":
             st.session_state['actual_anfis'] = actual_denorm
             # Tampilkan hasil prediksi
             st.subheader("📈 Hasil Prediksi (Setelah Denormalisasi)")
-            st.write(predictions+denorm)
+            st.write(predictions_denorm)
